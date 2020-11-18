82fc786aa925f54315235fbf0a7b084cc27cf9ac
LuisGallegos <albertogallego356@gmail.com>
2020-11-18 19:17:49 -0400

    method to check if NFC is available on a device. and import of libraries

--- app/src/main/java/com/example/nfctocare/MainActivity.java
+++ app/src/main/java/com/example/nfctocare/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.nfctocare;
  Degree project for the career of computer engineering
   * NFC application for the care of the elderly "NFC to CARE"
   * Workshop III
-  * EngineerProfessor Ronald Yeber Cruz Delgado
+  * Engineer Professor Ronald Yeber Cruz Delgado
   * GitHub repository
  *(hhttps://github.com/LuisGallegos25/NFC-to-CARE)
  * 18/11/2020
--- app/src/main/java/com/example/nfctocare/NFCManager.java
+++ app/src/main/java/com/example/nfctocare/NFCManager.java
@@ -1,5 +1,39 @@
 package com.example.nfctocare;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
+import android.support.design.widget.Snackbar;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Locale;
+
 public class NFCManager {
 
+    private Activity activiy;
+    private NfcAdapter nfcAdpt;
+
+    public NFCManager(Activity activity) {
+
+        this.activity = activity;
+    }
+
+    public void verifyNFC() throws NFCNotSupported, NFCNotEnabled {
+
+        nfcAdpt = NfcAdapter.getDefaultAdapter(activity);
+
+        if (nfcAdpt == null)
+            throw new NFCNotSupported();
+
+        if (!nfcAdpt.isEnabled())
+            throw new NFCNotEnabled();
+
+    }
 }

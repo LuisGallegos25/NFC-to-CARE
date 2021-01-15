2a4de1ebcd1f0b53496de2444682096c9f80c511
LuisGallegos <albertogallego356@gmail.com>
2021-01-15 19:59:54 -0400

    Creating the OpenHelper package for DB SQLite

---
+++ app/src/main/java/OpenHelper/SQLite_OpenHelper.java
@@ -0,0 +1,6 @@
+package OpenHelper;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SQLite_OpenHelper  extends SQLiteOpenHelper {
+
+}
---
+++ app/src/main/java/com/example/nfctocare/Registro.java
@@ -0,0 +1,4 @@
+package com.example.nfctocare;
+
+public class Registro {
+}

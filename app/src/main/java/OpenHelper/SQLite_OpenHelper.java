c5231510fe949359cf6f63b073a855c68cdffb73
LuisGallegos <albertogallego356@gmail.com>
2021-01-15 20:46:55 -0400

    "

--- app/src/main/java/OpenHelper/SQLite_OpenHelper.java
+++ app/src/main/java/OpenHelper/SQLite_OpenHelper.java
@@ -1,6 +1,48 @@
 package OpenHelper;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
 
 public class SQLite_OpenHelper  extends SQLiteOpenHelper {
 
+
+    public SQLite_OpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String query="create table usuario(id_usuario integer primary key autoincrement," +
+                "nombres text, usuario text, password text);";
+
+        sqLiteDatabase.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    //METODO PARA ABRIR DB
+    public void abrir(){
+        this.getWritableDatabase();
+    }
+
+    //METODO PARA CERRAR DB
+    public void cerrar(){
+        this.close();
+    }
+
+    //METODO PARA INSERTAR REGISTRO EN TABLA USUARIO
+    public void insertarRegistro(String nombres, String usuario, String password)
+    {
+        ContentValues valores=new ContentValues();
+        valores.put("Nombres",nombres);
+        valores.put("Usuario",usuario);
+        valores.put("Password",password);
+
+        this.getWritableDatabase().insert("usuario", null,valores);
+    }
 }
--- app/src/main/java/com/example/nfctocare/Registro.java
+++ app/src/main/java/com/example/nfctocare/Registro.java
@@ -1,4 +1,38 @@
 package com.example.nfctocare;
 
-public class Registro {
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import OpenHelper.SQLite_OpenHelper;
+
+public class Registro extends AppCompatActivity {
+
+    Button btnRegistro;
+    EditText txtNombres, txtUsuario, txtPassword;
+
+    SQLite_OpenHelper helper=new SQLite_OpenHelper(this,"nfc",null,1);
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro);
+
+        btnRegistro = (Button)findViewById(R.id.btn_reg);
+        txtNombres = (EditText)findViewById(R.id.nombres);
+        txtUsuario = (EditText)findViewById(R.id.user);
+        txtPassword = (EditText)findViewById(R.id.pass);
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
 }
